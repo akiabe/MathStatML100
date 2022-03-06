36c226928dcbcef14c22d32a4b3ca3ea9cb9df82
akiabe <62723205+akiabe@users.noreply.github.com>
2022-03-07 07:00:51 +0900

    Add files via upload

---
+++ src/R/1-2_重回帰.R
@@ -0,0 +1,10 @@
+# データ生成
+n = 100
+p = 2
+beta = c(1, 2, 3)
+x = matrix(rnorm(n*p), nrow=n, ncol=p)
+y = beta[1] + beta[2]*x[,1] + beta[3]*x[,2] + rnorm(n)
+X = cbind(1, x)
+
+# betaの推定
+solve(t(X) %*% X) %*% t(X) %*% y
